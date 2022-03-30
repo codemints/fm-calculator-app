86a9a87d67c9f160032772c3f7eabeeb55ef0af2
Daniel LittleThunder <lunaco5280@gmail.com>
2022-03-30 14:54:58 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import './styles/main.scss'
+import btnFunc from './scripts/buttons.js'
+import themeSwitcher from './scripts/theme.js'
+
+btnFunc();
+themeSwitcher();
---
+++ scripts/buttons.js
@@ -0,0 +1,137 @@
+const btnFunc = () => {
+  const buttons = Array.from(document.querySelectorAll('.key'));
+  const output = document.querySelector('.output h2');
+  let keyed = [];
+  let calc = [];
+  let i = 0;
+  let lastKey = undefined
+  let calcString = '';
+
+  const calculate = (lib) => {
+    // let terms = lib.filter((item, index) => index % 2 !== 0);
+    // let operation = lib.filter((item, index) => index % 2 === 0);
+    
+    lib.forEach(item => calcString += item.join(''));
+    
+    i = 0;
+    lastKey = undefined;
+    keyed.length = 0;
+    calc.length = 0;
+
+    return new Function('return ' + calcString)();
+  }
+
+  const parseData = (data, lib) => {
+    data.forEach((item, index, array) => {
+      lib.push(new Array());
+
+      if ( index !== array.length - 1 ) {
+        item.forEach((i, x, a) => {
+          if ( x !== a.length -1 ) {
+            lib[index].push(i);
+          }
+        })
+      }
+
+      if ( index === array.length - 1 ) {
+        item.forEach((i) => {
+          lib[index].push(i);
+        })
+      }
+    })
+
+    data.forEach((item, index, array) => {
+      if ( index !== array.length - 1 ) {
+        const arr = [];
+        arr.push(item[item.length - 1]);
+        lib.splice(index * 2 + 1, 0, arr);
+      }
+    })
+  }
+
+  const parseString = (data) => {
+    let calcString = '';
+    data.forEach(item => {
+      item.forEach(element => {
+        calcString += element;
+      })
+    })
+    return calcString;
+  }
+  
+  buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+
+      const targ = e.target;
+      const val = e.target.value;
+      
+      const dSet = targ.dataset.function;
+      const operators = ['+', '-', '*', '/'];
+
+      //Don't add non-number or non-operations signs to array
+      if ( !dSet ) {
+        if ( val === '.' && keyed.indexOf('.') !== -1 ) return;
+        if (keyed.length < 1 ) {
+          keyed.push(new Array(val));
+        } else {
+          keyed[i].push(val);
+        }
+      }
+      
+      //Delete everything in the array
+      if ( dSet === 'reset' ) {
+        keyed.length = 0;
+        i = 0;
+        lastKey = undefined;
+      }
+
+      //Fire calclation function and reset data contains more than 1 array
+      if ( dSet === 'submit' && keyed.length > 1 ) {
+        parseData(keyed, calc);
+        output.innerHTML = calculate(calc);
+        calcString = '';
+        return
+      }
+      
+      //Deal with operator keys
+      //If no data in array return
+      //Otherwise push to current array index and create a new array
+      if ( operators.indexOf(val) !== -1 ) {
+        if ( keyed.length < 1 ) {
+          lastKey = undefined;
+          return
+        }
+
+        if ( operators.indexOf(lastKey) !== -1 ) return;
+
+        keyed[i].push(val)
+        i++
+        keyed.push(new Array());
+      }
+      
+      //Delete last item
+      //If empty do nothing and/or reset
+      if ( dSet === 'delete' ) {        
+        if ( keyed.length < 1 || keyed[0].length < 1 ) {
+          keyed.length = 0;
+          i = 0;
+          lastKey = undefined;
+          output.innerHTML = '';
+          return
+        }
+        
+        if ( keyed[i].length < 1 ) {
+          i--;
+          keyed.pop();
+        }
+        keyed[i].pop();
+      }
+      
+      if ( dSet !== 'submit') output.innerHTML = parseString(keyed);
+      lastKey = val;
+    })
+
+  })
+}
+
+export default btnFunc
